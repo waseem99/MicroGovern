9ebaeee6608e9806f8f397707f62996d6876f398
Muhammad Waseem Khan <mwaseem99@live.com>
2015-05-16 02:57:28 +0500

    Services and Requests Controller and Views Added....

---
+++ MicroGovern/MicroGovern/Controllers/Services Management/Request mng/RequestsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MicroGovern.Models.Services_Management.Request_mng;
+
+namespace MicroGovern.Controllers.Services_Management.Request_mng
+{
+    public class RequestsController : Controller
+    {
+        private RequestDBContext db = new RequestDBContext();
+
+        // GET: Requests
+        public ActionResult Index()
+        {
+            return View(db.requests.ToList());
+        }
+
+        // GET: Requests/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Request request = db.requests.Find(id);
+            if (request == null)
+            {
+                return HttpNotFound();
+            }
+            return View(request);
+        }
+
+        // GET: Requests/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Requests/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,RequestIniated,Details,MinRate,MaxRate")] Request request)
+        {
+            if (ModelState.IsValid)
+            {
+                db.requests.Add(request);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(request);
+        }
+
+        // GET: Requests/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Request request = db.requests.Find(id);
+            if (request == null)
+            {
+                return HttpNotFound();
+            }
+            return View(request);
+        }
+
+        // POST: Requests/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,RequestIniated,Details,MinRate,MaxRate")] Request request)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(request).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(request);
+        }
+
+        // GET: Requests/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Request request = db.requests.Find(id);
+            if (request == null)
+            {
+                return HttpNotFound();
+            }
+            return View(request);
+        }
+
+        // POST: Requests/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Request request = db.requests.Find(id);
+            db.requests.Remove(request);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MicroGovern/MicroGovern/Models/Request mng/Request.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MicroGovern.Models.Services_Management.Request_mng
+{
+    public class Request
+    {
+        public Request ()
+        {
+
+        }
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime RequestIniated { get; set; }
+        public string Details { get; set; }
+        public decimal MinRate { get; set; }
+        public decimal MaxRate { get; set; }
+    }
+
+    public class RequestDBContext : DbContext
+    {
+        public DbSet<Request> requests { get; set; }
+    }
+}
