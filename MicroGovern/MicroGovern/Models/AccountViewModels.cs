806942f01c3875ffa8afc7b6617a587a0c374c0d
fzii97 <fzii97@hotmail.com>
2015-05-15 16:56:35 +0500

    phone number

--- MicroGovern/MicroGovern/Models/AccountViewModels.cs
+++ MicroGovern/MicroGovern/Models/AccountViewModels.cs
@@ -79,6 +79,11 @@ namespace MicroGovern.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "Phone")]
+        public string Phone { get; set; }
     }
 
     public class ResetPasswordViewModel
