f76ede8c917cfde4194f08c592ea155f4c6ef163
Muhammad Waseem Khan <mwaseem99@live.com>
2015-05-16 02:27:35 +0500

    Services added

--- MicroGovern/MicroGovern/Models/Services Management/Services mng/Service.cs
+++ MicroGovern/MicroGovern/Models/Services Management/Services mng/Service.cs
@@ -6,15 +6,20 @@ using System.Data.Entity;
 
 namespace MicroGovern.Models.Services_Management
 {
-    public class Services
+    public class Service
     {
         private int ID { get; set; }
         private string Title { get; set; }
         private DateTime DateAdded { get; set; }
         private string Details { get; set; }
-        public Services ()
+
+        public Service()
         {
 
+        }
+        public class ServiceDBContext : DbContext
+        {
+            public DbSet<Service> Services { get; set; }
         }
     }
 }
