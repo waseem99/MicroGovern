0eb65b75e81b9b67a656c448b7134ec877f19b9d
fzii97 <fzii97@hotmail.com>
2015-05-16 14:40:16 +0500

    User Registration

---
+++ MicroGovern/MicroGovern/Migrations/201505160930429_AccountType.cs
@@ -0,0 +1,16 @@
+namespace MicroGovern.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AccountType : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
