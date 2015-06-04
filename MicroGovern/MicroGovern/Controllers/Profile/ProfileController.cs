e3fcc4b9aa9b5713513b78b3f62d2293622c88ac
fzii97 <fzii97@hotmail.com>
2015-06-04 23:43:17 +0500

    Profile Photo Change added

--- MicroGovern/MicroGovern/Controllers/Profile/ProfileController.cs
+++ MicroGovern/MicroGovern/Controllers/Profile/ProfileController.cs
@@ -8,6 +8,7 @@ using MicroGovern.Models.Profile;
 using MicroGovern.Models;
 using System.Data.Entity;
 using MicroGovern.Models.Services_mng;
+using System.IO;
 
 namespace MicroGovern.Controllers.Profile
 {
@@ -71,6 +72,21 @@ namespace MicroGovern.Controllers.Profile
         }
 
         [System.Web.Mvc.HttpGet]
+        public JsonResult myProfile_updateNameAndDesc(String newName, String newDesc)
+        {
+            var userID = User.Identity.GetUserId();
+            UserDetails myDetails = db.usersdb.Single(x => x.ApplicationUserId == userID);
+
+            myDetails.FullName = newName;
+            myDetails.whoAmI = newDesc;
+
+            db.Entry(myDetails).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return Json("", JsonRequestBehavior.AllowGet);
+        }
+
+        [System.Web.Mvc.HttpGet]
         public JsonResult myProfile_deleteService(int serviceId)
         {
             Service newService = db.Services.Find(serviceId);
@@ -88,5 +104,69 @@ namespace MicroGovern.Controllers.Profile
             return Json("", JsonRequestBehavior.AllowGet);
         }
 
+        [HttpPost]
+        public ActionResult ProfilePicUpload(HttpPostedFileBase photo)
+        {
+            //string directory = @"~/Content/img/";
+
+            if (photo != null && photo.ContentLength > 0)
+            {
+                var userID = User.Identity.GetUserId();
+                UserDetails myDetails = db.usersdb.Single(x => x.ApplicationUserId == userID);
+                //var fileName = Path.GetFileName(photo.FileName);
+                var fileName = myDetails.ID + "_profile_" + Path.GetFileName(photo.FileName);
+                var previousDP = myDetails.profilePicURL;
+                string path = System.IO.Path.Combine(Server.MapPath("~/Content/img/profile"), fileName);
+                photo.SaveAs(path);
+
+                
+                myDetails.profilePicURL = "/Content/img/profile/" + fileName;
+
+                db.Entry(myDetails).State = EntityState.Modified;
+                db.SaveChanges();
+
+                string fullPath = Request.MapPath("~" + previousDP);
+                if (System.IO.File.Exists(fullPath) && previousDP != myDetails.profilePicURL)
+                {
+                    System.IO.File.Delete(fullPath);
+                    //Session["DeleteSuccess"] = "Yes";
+                }
+                
+            }
+
+            return RedirectToAction("myProfile_edit");
+        }
+        /*
+        [HttpPost]
+        public ActionResult ProfilePicUpload(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                string pic = System.IO.Path.GetFileName(file.FileName);
+                string path = System.IO.Path.Combine(Server.MapPath("~/Content/img/profile"), pic);
+                // file is uploaded
+                file.SaveAs(path);
+
+                var userID = User.Identity.GetUserId();
+                UserDetails myDetails = db.usersdb.Single(x => x.ApplicationUserId == userID);
+                myDetails.profilePicURL = path;
+
+                db.Entry(myDetails).State = EntityState.Modified;
+                db.SaveChanges();
+
+                // save the image path path to the database or you can send image
+                // directly to database
+                // in-case if you want to store byte[] ie. for DB
+                /*using (MemoryStream ms = new MemoryStream())
+                {
+                    file.InputStream.CopyTo(ms);
+                    byte[] array = ms.GetBuffer();
+                }*/
+
+            }
+            // after successfully uploading redirect the user
+           /* return RedirectToAction("myProfile_edit");
+            //return RedirectToAction("actionname", "controller name");
+        }*/
+
     }
-}
--- MicroGovern/MicroGovern/Scripts/ProfilePage.js
+++ MicroGovern/MicroGovern/Scripts/ProfilePage.js
@@ -1,5 +1,10 @@
 ﻿$(document).ready(function () {
     populateSubServicesDropdown();
+    $("#collapseExample").slideUp();
+    $("#saveDesc").hide();
+    $("#profileDescEdit").hide();
+    $("#userNameEditdiv").hide();
+
     //When main category dropdown changes in add new service
     $("#majorcategory").change(function () {
         populateSubServicesDropdown();
@@ -87,8 +92,8 @@
         });
     });
 
-    $("#AddNewServiceBtn").click(function () {
-        
+    $("#addmorebtn").click(function () {
+        $("#collapseExample").toggle();
     });
 
     function populateSubServicesDropdown() {
@@ -116,6 +121,77 @@
             }
         });
     }
+
+    /*document.getElementById("editDesc").onclick = function () {
+        if (document.getElementById('editDesc').innerHTML == "Edit")
+            writeData();
+        else if (document.getElementById('editDesc').innerHTML == "Save")
+            saveEdit();
+    }*/
+
+    
+
+    $("#editDesc").click(function () {
+        $("#editDesc").hide();
+        $("#profileDesc").hide();
+        $("#userNamediv").hide();
+
+        var profiledesc = $("#profileDesc").text();
+        $("#profileDescEdit").val(profiledesc);
+        var uname = $("#userNameOld").text();
+        $("#userNewName").val(uname);
+
+        $("#saveDesc").show();
+        $("#profileDescEdit").show();
+        $("#userNameEditdiv").show();
+    });
+
+    $("#saveDesc").click(function () {
+        $("#saveDesc").hide();
+        $("#profileDescEdit").hide();
+        $("#userNameEditdiv").hide();
+
+        var newName = $("#userNewName").val();
+        var newDesc = $("#profileDescEdit").val();
+        
+        $.ajax({
+            type: "GET",
+            url: "/profile/myProfile_updateNameAndDesc",
+            data: { newName: newName, newDesc: newDesc },
+            contentType: "application/json;charset=utf-8",
+            dataType: "json",
+            success: function (result) {
+                debugger;
+                $("#profileDesc").text(newDesc);
+                $("#userNameOld").text(newName);
+
+                $("#editDesc").show();
+                $("#profileDesc").show();
+                $("#userNamediv").show();
+            },
+            error: function (response) {
+                debugger;
+                console.log('eror' + response);
+            }
+        });
+
+        
+    });
+
+    $("#changeDP").click(function () {
+        //$("#collapseExample").toggle();
+        console.log("change dp div");
+        $("#newIMGinput").trigger('click');
+    });
+
+    $("#newIMGinput").change(function () {
+        //$("#collapseExample").toggle();
+        console.log("sadasds22222222222ad");
+        if ($("#newIMGinput").val()) {
+            console.log("sadasdsad");
+            $("#newIMGsubmit").trigger('click');
+        }
+    });
 });
 
 
@@ -126,62 +202,9 @@ window.onload = function () {
         //else if (document.getElementById('editDesc')=="Save")
         //    document.getElementById("editDesc").onclick = saveEdit;
 
-        document.getElementById("editDesc").onclick = function () {
-            if (document.getElementById('editDesc').innerHTML == "Edit")
-                writeData();
-            else if (document.getElementById('editDesc').innerHTML == "Save")
-                saveEdit();
-        }
-    }
-
-
-
-
-function writeData() {
-    var butt = document.getElementById('editDesc');
-    butt.innerHTML="Save";
         
-    var obj = document.getElementById("profileDesc");
-        
-    while (obj.nodeType != 1) {
-        obj = obj.parentNode;
     }
-    if (obj.tagName == 'TEXTAREA' || obj.tagName == 'A') return;
-    while (obj.nodeName != 'P' && obj.nodeName != 'HTML') {
-        obj = obj.parentNode;
-    }
-    if (obj.nodeName == 'HTML') return;
-    var x = obj.innerHTML;
-    var y = document.createElement('TEXTAREA');
-    y.style.height = "100px";
-    y.style.width = "80%";
-    var z = obj.parentNode;
-    z.insertBefore(y, obj);
-    z.insertBefore(butt, obj);
-    z.removeChild(obj);
-    y.value = x;
-    y.focus();
-    editing = true;
 
 
-    //document.getElementById("profileDesc").innerHTML = "Changed Paragraph Content";
-    //return false;
-}
 
-function saveEdit() {
-    window.location.reload();
 
-    //var area = document.getElementsByTagName('TEXTAREA')[0];
-    //var y = document.createElement('P');
-    //y.className = "whitetext";
-    //y.id = "profileDesc";
-    //document.getElementById('editDesc').innerHTML = "Edit";
-    //var name = document.getElementById('FullName');
-    //var z = area.parentNode;
-    //y.innerHTML = area.value;
-    //z.insertBefore(y, area);
-    
-    //z.removeChild(area);
-    ////z.removeChild(document.getElementsByID('editDesc')[0]);
-    //editing = false;
-}
