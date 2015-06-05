e1dcdcf0b2449bc140162662b4a16c87e97e49e7
ar4ahmad <ar4ahmad@gmail.com>
2015-06-05 16:02:32 +0500

    Need to commit before I can pull...-.- This includes incomplete search page...-.-

---
+++ MicroGovern/MicroGovern/Scripts/SearchResult.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+    $("#SearchFilterCollapse").slideUp();
+
+    $("#SearchFilterBtn").click(function () {
+        $("#SearchFilterCollapse").toggle();
+    });
+
+    $('.dropdown-toggle').dropdown('toggle');
+
+});
+
+$(function () {
+
+    $("#slider-range").slider({
+
+        range: true,
+
+        min: 0,
+
+        max: 500,
+
+        values: [75, 300],
+
+        slide: function (event, ui) {
+
+            $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+
+        }
+
+    });
+
+    $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+
+      " - $" + $("#slider-range").slider("values", 1));
+
+});
