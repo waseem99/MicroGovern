b601137860c9c697dc9b7d615a7e5946049ad9c9
waseem99 <mwaseemkha@gmail.com>
2015-06-10 10:13:56 +0500

    Notifications added.

---
+++ MicroGovern/MicroGovern/Models/Notifications/AUserNotifications.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MicroGovern.Models.Notifications
+{
+    public abstract class AUserNotifications
+    {
+        protected AUserNotifications successor;
+
+        public void SetSuccessor(AUserNotifications succ)
+        {
+            this.successor = succ;
+        }
+
+        public abstract void HandleRequest(int request);
+    }
+}
---
+++ MicroGovern/MicroGovern/Models/Notifications/SMSNotifications.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MicroGovern.Models.Notifications
+{
+    public class SMSNotifications: AUserNotifications
+    {
+        public override void HandleRequest(int request)
+    {
+        successor.HandleRequest(request);
+    }
+}
+}
